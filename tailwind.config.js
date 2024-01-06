d16a6398dfa7dbaf979959bae5fcc574186c53f7
Jiruschai Anuntabundit <hamjirus@gmail.com>
2024-01-06 14:30:11 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Pixelify: ["Pixelify Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
